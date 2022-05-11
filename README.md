ab220ed0fb7645ccacac939f61ec1bdd08395d29
Johnny Chen <johnnychen94@hotmail.com>
2022-05-11 18:07:53 +0800

    introduce lazy macro

--- README.md
+++ README.md
@@ -10,14 +10,17 @@ module MyPkg
 using Base: invokelatest
 using LazyModules
 
-include("examples/LazyImageCore.jl")
-using .LazyImageCore
+@lazy import ImageCore
 
 function rand_image(sz)
     return invokelatest(rand, ImageCore.RGB, sz)
 end
 
-export rand_image
+function black_pixel()
+    return invokelatest(ImageCore.RGB, 0.0, 0.0, 0.0)
+end
+
+export rand_image, black_pixel
 
 end
 ```
@@ -26,5 +29,9 @@ end
 @time using .MyPkg # 0.001461 seconds
 
 # the actual loading is delayed to the first usage of the package
-img = @time rand_image((64, 64)) # 0.710679 seconds
+img = @time rand_image((64, 64)); # 0.710679 seconds
+
+using Colors
+@btime black_pixel() # ~100ns
+@btime zero(RGB) # ~1ns
 ```
--- src/LazyModules.jl
+++ src/LazyModules.jl
@@ -1,6 +1,6 @@
 module LazyModules
 
-export LazyModule
+export lazy_import, @lazy
 
 using Base: PkgId
 
@@ -34,6 +34,45 @@ function Base.getproperty(m::LazyModule, s::Symbol)
     end
     lm = Base.root_module(getfield(m, :_lazy_pkgid))
     return getfield(lm, s)
+end
+
+"""
+    @lazy import PkgName
+
+Lazily import package `PkgName`.
+
+The package loading of `PkgName` will be delayed to when it's actually required.
+For instance, if one does `@lazy import ImageCore`, then any symbol usage such
+as `ImageCore.RGB` will trigger the package loading.
+
+!!!info "dispatch overhead"
+    This strategy uses `invokelatest` to work around the world age issues, it
+    has about 100ns overhead for each call. Thus it should only be used for
+    non-trivial function call.
+
+"""
+macro lazy(ex)
+    if ex.head != :import
+        @warn "only `import` command is supported"
+        return ex
+    end
+    args = ex.args
+    if length(args) != 1
+        @warn "only single package import is supported"
+        return ex
+    end
+    x = args[1]
+    if x.head != :.
+        return ex
+    end
+    pkgname = String(x.args[1])
+    m = lazy_import(pkgname)
+    Core.eval(__module__, :($(x.args[1]) = $m))
+end
+
+function lazy_import(pkgname::String)
+    pkgid = Base.identify_package(pkgname)
+    return LazyModule(pkgid)
 end
 
 end # module
